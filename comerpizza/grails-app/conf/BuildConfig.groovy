5c598d52acc7f304ab947524044c170c556f8909
diegofaria <diegofndefaria@gmail.com>
2013-08-12 11:35:41 -0300

    teste

--- comerpizza/grails-app/conf/BuildConfig.groovy
+++ comerpizza/grails-app/conf/BuildConfig.groovy
@@ -41,7 +41,7 @@ grails.project.dependency.resolution = {
 
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
-
+        //teste 
         // runtime 'mysql:mysql-connector-java:5.1.22'
     }
 
